6064788763c3006c65937029de9055f6b0f614d7
MarcosRamiro <mrsantos2006@hotmail.com>
2025-02-09 15:19:07 -0300

    fix: auth google 6

--- src/login/login.js
+++ src/login/login.js
@@ -1,13 +1,13 @@
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
+function handleToken(token) {
+    // var profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId());
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail());
   
-    var id_token = googleUser.getAuthResponse().id_token;
+    // var id_token = googleUser.getAuthResponse().id_token;
     // Envie o id_token para seu servidor para validação
 
-    console.log(id_token);
+    console.log(token);
   }
